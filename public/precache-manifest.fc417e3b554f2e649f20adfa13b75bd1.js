67c0b8db3f49c00bd81246b466dd2ede3e6047a0
Janis Müller <janis_mueller@gmx.de>
2020-11-14 12:08:35 +0100

    Add files via upload

---
+++ public/precache-manifest.fc417e3b554f2e649f20adfa13b75bd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf61a505e48a7f807742504689e5bfb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94e7e2d5b90379992598",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e39e20560496ac174d2f",
+    "url": "/static/js/2.b2052e67.chunk.js"
+  },
+  {
+    "revision": "e2d3cf8a14fcfd388838315d6db21d45",
+    "url": "/static/js/2.b2052e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e7e2d5b90379992598",
+    "url": "/static/js/main.c008d386.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
